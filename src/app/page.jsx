198e02a93a5b887bede7c8e08f3dfe5a5f7cb2f3
amanuel-hailu <amanuel.hailu@email.sitel.org>
2023-09-30 23:44:08 -0400

    added static assets and styles

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,6 +1,25 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import Hero from "public/hero.png";
+import Button from "@/components/button/Button";
 
 export default function Home() {
-  return <div className={styles.container}>HOME</div>;
+  return (
+    <div className={styles.container}>
+      <div className={styles.item}>
+        <h1 className={styles.title}>
+          Better design for your digital products
+        </h1>
+        <p className={styles.desc}>
+          Turning your idea into a reality. We bring together the teams from the
+          global tech industry.
+        </p>
+
+        <Button text="See Our Work" url="/portfolio" />
+      </div>
+      <div className={styles.item}>
+        <Image src={Hero} alt="hero" className={styles.img} />
+      </div>
+    </div>
+  );
 }
--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,10 +1,41 @@
 import React from "react";
+import styles from "./footer.module.css";
+import Image from "next/image";
 
 const Footer = () => {
   return (
-    <div>
+    <div className={styles.container}>
       <div>©2023 Amanuel. All rights reserved.</div>
-      <div></div>
+      <div className={styles.social}>
+        <Image
+          src="/facebook.png"
+          width={20}
+          height={20}
+          className={styles.icon}
+          alt="facebook"
+        />
+        <Image
+          src="/youtube.png"
+          width={20}
+          height={20}
+          className={styles.icon}
+          alt="youtube"
+        />
+        <Image
+          src="/instagram.png"
+          width={20}
+          height={20}
+          className={styles.icon}
+          alt="instagram"
+        />
+        <Image
+          src="/twitter.png"
+          width={20}
+          height={20}
+          className={styles.icon}
+          alt="twitter"
+        />
+      </div>
     </div>
   );
 };
